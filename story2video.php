9514c56c2d89b19104c26c85d69e1622f4a55f06
Angel Cee <39429664+JossyMall@users.noreply.github.com>
2024-06-27 00:03:13 -0700

    Update story2video.php

--- story2video.php
+++ story2video.php
@@ -5,6 +5,11 @@ Description: Export Google Web Stories to video format.
 Version: 1.0
 Author: Your Name
 */
+function story2video_enqueue_scripts() {
+    wp_enqueue_script('story2video-script', plugin_dir_url(__FILE__) . 'js/story2video.js', array('jquery'), null, true);
+}
+add_action('admin_enqueue_scripts', 'story2video_enqueue_scripts');
+
 
 // Exit if accessed directly
 if (!defined('ABSPATH')) {

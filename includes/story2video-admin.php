0ecb2a7620317a39286c434929eed5a321f87525
Angel Cee <39429664+JossyMall@users.noreply.github.com>
2024-06-27 00:00:16 -0700

    Update story2video-admin.php

--- includes/story2video-admin.php
+++ includes/story2video-admin.php
@@ -14,13 +14,19 @@ function story2video_admin_menu() {
 }
 
 function story2video_admin_page() {
+    $stories = get_posts(array('post_type' => 'web-story', 'numberposts' => -1));
     ?>
     <div class="wrap">
         <h1>Story2Video</h1>
-        <form method="post" action="options.php">
-            <?php settings_fields('story2video_options_group'); ?>
-            <?php do_settings_sections('story2video'); ?>
-            <?php submit_button(); ?>
+        <form method="post" action="<?php echo admin_url('admin-post.php'); ?>">
+            <input type="hidden" name="action" value="story2video_export">
+            <label for="story_id">Select a Web Story:</label>
+            <select name="story_id" id="story_id">
+                <?php foreach ($stories as $story): ?>
+                    <option value="<?php echo $story->ID; ?>"><?php echo $story->post_title; ?></option>
+                <?php endforeach; ?>
+            </select>
+            <?php submit_button('Export to Video'); ?>
         </form>
     </div>
     <?php

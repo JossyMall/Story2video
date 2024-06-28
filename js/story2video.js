ba76e8a8308ca9e465102556d96fbcc37fd9f4db
Angel Cee <39429664+JossyMall@users.noreply.github.com>
2024-06-28 07:04:18 -0700

    Update story2video.js

--- js/story2video.js
+++ js/story2video.js
@@ -1,14 +1,39 @@
+Here is the complete JavaScript code for the `story2video.js` file:
+
+```javascript
 jQuery(document).ready(function($) {
-    $('form').submit(function() {
+    // Handle FFmpeg path testing
+    $('#test-ffmpeg-path').on('click', function() {
+        var ffmpegPath = $('input[name="story2video_ffmpeg_path"]').val();
+        $.ajax({
+            url: story2video_ajax.ajax_url,
+            type: 'POST',
+            data: {
+                action: 'test_ffmpeg_path',
+                ffmpeg_path: ffmpegPath
+            },
+            success: function(response) {
+                if (response.success) {
+                    $('#ffmpeg-test-result').html('<p style="color: green;">' + response.data + '</p>');
+                } else {
+                    $('#ffmpeg-test-result').html('<p style="color: red;">' + response.data + '</p>');
+                }
+            }
+        });
+    });
+
+    // Handle export progress
+    $('form').on('submit', function() {
         $('#export-progress').show();
         var progressBar = $('#progress-bar');
+        var progressValue = 0;
         var interval = setInterval(function() {
-            var value = progressBar.val();
-            if (value < 100) {
-                progressBar.val(value + 10);
-            } else {
+            progressValue += 10;
+            progressBar.val(progressValue);
+            if (progressValue >= 100) {
                 clearInterval(interval);
             }
-        }, 1000); // Update every second
+        }, 500);
     });
 });
+```
